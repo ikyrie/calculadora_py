5e2862786bd3691e4c229799fdfcf55d2fbd2706
ikyrie <mah.c5r@gmail.com>
2023-04-04 16:20:30 -0300

    Mudando funções para snake case e finalizando o README

---
+++ README.md
@@ -0,0 +1,61 @@
+# Projeto Calculadora em terminal com Python3
+
+## Tecnologias utilizadas:
+
+- Python3 🐍
+- Standard Python library **unittest** for testing
+
+## O que é
+
+Uma calculadora feita em Python3 que tem as seguintes funcionalidades:
+
+- Soma;
+- Subtração;
+- Multiplicação;
+- Divisão
+
+A calculadora roda no terminal e recebe input (entrada) do usuário para executar suas funcionalidades.
+
+(colocar imagem)
+
+Primeiro é pedido no terminal que a pessoa digite o primeiro valor. Os valores devem ser **inteiros**.
+
+Depois a pessoa deve selecionar a operação passando uma *string* ou um texto com uma das seguintes opções: `+ - * /` respectivamente.
+
+Por último a pessoa digita o segundo valor seguindo a mesma regra do primeiro.
+
+Por fim, o resultado será exibido na última linha do terminal, antes do final da execução do programa.
+
+> O resultado pode ser um *float* ou um *int*.
+
+O programa foi testado utilizando a biblioteca padrão do próprio Python3 -> **unittest**.
+
+As entradas do usuário também são tratadas e validadas antes da execução final da operação.
+
+## Como rodar?
+
+Para rodar a aplicação é necessário ter pelo menos a versão 3.10.0 do Python (versão que foi produzido o programa).
+
+Execute o programa com o seguinte comando em seu terminal favorito:
+
+```shell
+python3 calculadora.py
+```
+
+Para rodar os testes, execute o seguinte comando:
+
+```shell
+python3 test.py -v
+```
+
+> Utilizamos o argumento `-v` para melhorar a verbosiadade dos testes em execução.
+
+## Objetivos deste projeto
+
+Este projeto foi montado como o intúito de aprender Python3. Algumas coisas que eu queria exercitar com este projeto:
+
+- Classes;
+- Testes;
+- Métodos;
+- Básico da linguagem;
+- Boas práticas;
--- calculadora.py
+++ calculadora.py
@@ -22,24 +22,24 @@ class Calculadora:
         return a * b
 
     @staticmethod
-    def imprimeResultado(resultado):
+    def imprime_resultado(resultado):
         print(resultado)
 
     def calcula(self):
         match self.operacao:
             case "+":
-                self.imprimeResultado(self.soma(self.a, self.b))
+                self.imprime_resultado(self.soma(self.a, self.b))
 
             case "-":
-                self.imprimeResultado(self.subtracao(self.a, self.b))
+                self.imprime_resultado(self.subtracao(self.a, self.b))
             
             case "/":
-                self.imprimeResultado(self.divisao(self.a, self.b))
+                self.imprime_resultado(self.divisao(self.a, self.b))
 
             case "*":
-                self.imprimeResultado(self.multiplicacao(self.a, self.b))
+                self.imprime_resultado(self.multiplicacao(self.a, self.b))
 
-def recebeValor():
+def recebe_valor():
     """
     Essa função pega o input do usuário e valida se o valor recebido é um inteiro.
     """
@@ -47,9 +47,9 @@ def recebeValor():
         return int(input('Digite um valor: '))
     except:
         print('Valor inválido! Tente novamente')
-        return recebeValor()
+        return recebe_valor()
 
-def recebeOperacao():
+def recebe_operacao():
     """
     Essa função pega o input do usuário e valida se a operação recebida é uma string e pertence ao conjunto de operações válidas
     """
@@ -61,12 +61,12 @@ def recebeOperacao():
         return inputDoUsuario
     except:
         print('Operação inválida! Tente novamente')
-        return recebeOperacao()
+        return recebe_operacao()
 
 def main():
-    a = recebeValor()
-    op = recebeOperacao()
-    b = recebeValor()
+    a = recebe_valor()
+    op = recebe_operacao()
+    b = recebe_valor()
 
     minhaCalculadora = Calculadora(a, op, b)
     minhaCalculadora.calcula()
